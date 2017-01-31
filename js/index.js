443942d110d4833d15bf0fa36b88f7e3f1c19e9c
derpachu <nomnom148>
2017-01-31 17:18:16 -0500

    added unique sounds for hover

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
 var basebuttonElement = document.getElementById("base");
 
 basebuttonElement.addEventListener("click",function(){document.getElementById("Base").play()});
-basebuttonElement.addEventListener("mouseenter",function(){document.getElementById("Base").play()});
+basebuttonElement.addEventListener("mouseenter",function(){document.getElementById("Kick").play()});
 
 var basebuttonElement = document.getElementById("clap");
 
 basebuttonElement.addEventListener("click",function(){document.getElementById("Clap").play()});
-basebuttonElement.addEventListener("mouseenter",function(){document.getElementById("Clap").play()});
+basebuttonElement.addEventListener("mouseenter",function(){document.getElementById("Snare").play()});
 
 var basebuttonElement = document.getElementById("hihat");
 
 basebuttonElement.addEventListener("click",function(){document.getElementById("Hihat").play()});
-basebuttonElement.addEventListener("mouseenter",function(){document.getElementById("Hihat").play()});
+basebuttonElement.addEventListener("mouseenter",function(){document.getElementById("Openhat").play()});
